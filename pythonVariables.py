11b9b1783a376e9cecbd2d0c1c3249a42b88ddb4
Isme2314 <111314531+Isme2314@users.noreply.github.com>
2022-08-31 15:01:35 -0600

    Update pythonVariables.py

--- pythonVariables.py
+++ pythonVariables.py
@@ -1,50 +1,65 @@
-#Print command
-x = 5
-y = "John"
-print(x)
-print(y)
-# words in programming are called strings
-# 5 = integer => int
-#"hello"= word => string
-# booleans = 0,1 example true/false
+# Functions
+'''A function is a block of code which only runs when it is called. You can pass data, known as parameter, into a function. A function can get data as a result'''
+# Creating a function
+# In python a function is created using the def keyword
+def my_function (fname):
+  print (fname + " Joaquin")
+my_function("pizza")
+# Calling a Function
+# To call a function, use the function name followed by parenthesis
+def my_Function (fname):
+  print (fname + "Joaquin")
+my_Function ("pizza")
+# Parameters or arguments
+# The terms parameter and argument can be used for the same thing; info that are passed into a function
+# A parameter is the variable listed inside the parenthesis in the function definition
+# An argument is the value that is sent to the function when it is called
+def my_function (fname,lname):
+  print(fname + " " + lname)
+my_function("Carlos" , "Campuzano")
+# Python Class
+# To create a class, use keyword class
+# create a class named MyClass, with a property named x:
+class MyClass :
+  x=5
 
-# variables are case sensitive
-a = 4
-A = "Bob"
-print (a,A)
-# A WILL NOT OVERWRITE a
-# = in programming means "Assigned to" Exple 5 is assigned to variable x
+# Create an object
+# Now we can use the class name MyClass to create an object
+# Create an object named g1, and print the value of x
+g1 = MyClass ()
+print (g1.x)
+# The _init_ () function
+# Use the underscore _init_ () function to assign values for name and age :
 
-# LEGAL VARIABLE NAMES
-# A variable name must start or underscore character
-# IT CANNOT START WITH A NUMBER
-# IS CASE SENSITIVE AND CANNOT CONTAIN ALPHA-NUMERIC CHARACTERS (a-z, 0-9)
-# CAN ALSO MEAN "NOT EQUAL TO"(AGE IS # age)
-# python allow you to assign many values to multiple variable in 1 line
-X,Y,Z= "Orange", "Banana", "Cherry"
-print(X)
-print(Y)
-print(Z)
-
-# python allows you to assign 1 value to multiple variables
-X = Y = Z = "Orange"
-print(X)
-print(Y)
-print(Z)
-
-#RANDOM NUMBER
-import random
-print(random.randrange(1,10))
-
-#concatenation
-#concatenation is to combine two strings, you can use te + operator
-a="hello"
-B="Patricia"
-c=a+B
-print(c)
-#add space between variables
-c = a + " " + B
-#string format
-age = "36"
-text = "My name is Patricia, I am "+ age
-print (text)
+# Example create a class named Person, use the _init_(function) to assign values for name and age
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+p1=Person ("john", 36)
+print (p1.name)
+print (p1.age)
+# note the _init_() function is called automatically everytime the class is being used to create a new object
+# Python Inheritance
+# Inheritance allows us to define a class that inherit all the methods and properties from anoth class
+# Parent class is the class being inherited from also called base class
+# Child class is : the class that inherits from another class, also called derived class
+# Any class can be parent, so syntax is the same as creating another class 
+# ex: create a class named Person, with firstname and lastname properties, and a printname method
+class Person: #Parent class
+  def _init_ (self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+def print (self):
+  print (self.firstname, self.lastname)
+  # use the person class to create an object, and then execute the printname method
+x= Person ("john", "doe")
+x.printname()
+#child class
+# to create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class
+# ex. assignment prompt: create a class named student which will inherit the properties and methods from the Person class
+class Student (Person):
+  pass
+#ex: use the student class to create and object, and then execute the printname method 
+  x = student ("Mike","Olsen")
+  x.printname()
